3fcc6b5782a1f28267157b7aee028740cf8fd6e9
shivam <64252021+itsprogrammingwithme@users.noreply.github.com>
2023-08-14 21:40:14 +0530

    added dynamic db config

--- DB/Mysql.js
+++ DB/Mysql.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const Mysqldb = mysql.createPool({
-  host: 'localhost',     // Replace with your MySQL host
-  user: 'root',          // Replace with your MySQL username
-  password: '',  // Replace with your MySQL password
-  database: 'localtraval',      // Replace with your MySQL database name
+  host: process.env.DB_HOST, // Replace with your host name
+  user: process.env.DB_USER,// Replace with your MySQL username
+  password: process.env.DB_PASS,  // Replace with your MySQL password
+  database: process.env.DB_NAME||"",      // Replace with your MySQL database name
   connectionLimit: 10    // Set connection limit (optional)
 });
 
